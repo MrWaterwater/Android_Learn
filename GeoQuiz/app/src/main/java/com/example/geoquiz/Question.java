ec420d6050108f45a11b7d0e15a4b48653722450
WangShuishui <1327822679@qq.com>
2020-12-28 23:39:23 +0800

    第一个项目提交

---
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/Question.java
@@ -0,0 +1,27 @@
+package com.example.geoquiz;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getmTextResId() {
+        return mTextResId;
+    }
+
+    public void setmTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public boolean ismAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setmAnswerTrue(boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public Question(int mTextResId, boolean mAnswerTrue){
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+}
